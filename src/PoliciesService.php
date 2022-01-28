158a8ba828cd62bd6f8cacdaf043166fc4abfbaa
Rudie Dirkx <github@hotblocks.nl>
2022-01-28 21:09:24 +0100

    Initial

---
+++ src/PoliciesService.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace rdx\authgate;
+
+use Illuminate\Contracts\Auth\Access\Gate;
+use Illuminate\Support\Str;
+use ReflectionClass;
+use ReflectionMethod;
+
+class PoliciesService {
+
+  public $gate;
+  public $cacheFile = null;
+  public $policyClasses = [];
+
+  public function __construct(Gate $gate) {
+    $this->gate = $gate;
+  }
+
+  public function setClasses(array $classes) : void {
+    $this->policyClasses = $classes;
+  }
+
+  public function setCachePath(string $filepath) : void {
+    $this->cacheFile = $filepath;
+  }
+
+  public function disableGuessPolicyNames() : void {
+    $this->gate->guessPolicyNamesUsing(function($class) {
+      return null;
+    });
+  }
+
+  public function isCached() : bool {
+    return file_exists($this->cacheFile);
+  }
+
+  public function defineAbilities(bool $useCache = true) : void {
+    $abilities = $this->getAbilities($useCache);
+    foreach ($abilities as $ability => $callable) {
+      $this->gate->define($ability, $callable);
+    }
+  }
+
+  public function getAbilities(bool $useCache = true) : array {
+    if ($useCache && $this->isCached()) {
+      return require $this->cacheFile;
+    }
+    return $this->getLiveAbilities();
+  }
+
+  public function getLiveAbilities() : array {
+    $abilities = [];
+    foreach ($this->policyClasses as $class) {
+      $reflected = new ReflectionClass($class);
+      foreach ($reflected->getMethods() as $method) {
+        if ($this->includeMethod($method)) {
+          $abilities[$this->getAbilityName($method)] = $class . '@' . $method->getName();
+        }
+      }
+    }
+    return $abilities;
+  }
+
+  protected function getAbilityName(ReflectionMethod $method) : string {
+    return Str::kebab(str_replace('_', '-', $method->getName()));
+  }
+
+  protected function includeMethod(ReflectionMethod $method) : bool {
+    return $method->isPublic() && strpos($method->getName(), '__') !== 0;
+  }
+
+}
---
+++ src/PoliciesServiceProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace rdx\authgate;
+
+use Illuminate\Support\ServiceProvider;
+
+class PoliciesServiceProvider extends ServiceProvider {
+
+  protected $policies = [];
+  protected $useCache = true;
+  protected $disableGuessPolicyNames = true;
+  protected $cacheStoragePath = 'framework/abilities.php';
+
+  public function register() {
+    $this->app->singleton(PoliciesService::class);
+  }
+
+  public function boot(PoliciesService $policies) {
+    // $t = microtime(1);
+
+    if ($this->disableGuessPolicyNames) {
+      $policies->disableGuessPolicyNames();
+    }
+
+    $policies->setCachePath(storage_path($this->cacheStoragePath));
+    $policies->setClasses($this->policies);
+    $policies->defineAbilities($this->useCache);
+
+    // $t = 1000 * (microtime(1) - $t);
+  }
+
+}
